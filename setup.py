338942d78cdf819e84f5b46f74e5252a304f59e6
takemi.ohama <takemi.ohama@gmail.com>
2023-10-30 03:19:02 +0900

    debug

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def _test_requirements():
 
 with open(path.join(root_dir, package_name, '__init__.py')) as f:
     init_text = f.read()
-    version = re.search(r'__version__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+    version = re.search(r'__VERSION__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
     license = re.search(r'__license__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
     author = re.search(r'__author__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
     author_email = re.search(r'__author_email__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
